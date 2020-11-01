36144baca8f157783962c7b680fba0f47e631ca3
Himanshu Dabas <20219940+himanshudabas@users.noreply.github.com>
2020-11-01 17:24:02 +0000

    fix: fallback method for quiz scraping
    when the default method fails to fetch the quiz, fallback method will be used instead

--- scrape.py
+++ scrape.py
@@ -2,6 +2,9 @@ from scraping.gktoday_quiz import quiz_run
 from scraping.gktoday_magazine import magazine_run
 from scraping.app_md import init_app_metadata
 
-quiz_run()
+try:
+    quiz_run()
+except TypeError:
+    quiz_run(True)
 magazine_run()
 init_app_metadata()
