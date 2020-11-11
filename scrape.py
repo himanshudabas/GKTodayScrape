e031aab82af16699f1abbea698790c9d634a7dee
Himanshu Dabas <20219940+himanshudabas@users.noreply.github.com>
2020-11-11 14:52:06 +0000

    added temporary fix for magazine scraping
    while the quiz scraper is still broken, magazine scraper is
    still functional, thus added a try except block to keep the
    magazine scraper working even when quiz scrapers breaks

--- scrape.py
+++ scrape.py
@@ -2,9 +2,14 @@ from scraping.gktoday_quiz import quiz_run
 from scraping.gktoday_magazine import magazine_run
 from scraping.app_md import init_app_metadata
 
+
 try:
     quiz_run()
 except TypeError:
-    quiz_run(True)
+    try:
+        quiz_run(True)
+    # temporary: if quiz fails, magazine would still function correctly
+    except Exception:
+        pass
 magazine_run()
 init_app_metadata()
